0ceb102b15a32d5837942fd7c2647469a079ba29
KamelABA <123214040+KamelABA@users.noreply.github.com>
2025-03-11 22:54:36 +0100

    user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\File;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // قراءة ملف JSON
+        $json = File::get(database_path('seeders/user.json'));
+        $users = json_decode($json, true);
+
+        // إدراج البيانات في جدول users
+        foreach ($users as $user) {
+            DB::table('users')->insert([
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'password' => Hash::make($user['password']),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
