0d09f4d1d992a6e1b2c7c832874d109ef5e7caa9
KamelABA <123214040+KamelABA@users.noreply.github.com>
2025-03-11 19:43:01 +0100

    array

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -33,7 +33,7 @@ class ProductSeeder extends Seeder
                 'price'       => $record['price'],
                 'quantity'       => $record['quantity'],
                 'type' => $record['type'],
-                'images' => $record['images'],
+                'images' => json_encode(explode(',', $record['images'])),
                 'created_at'  => $record['created_at'],
                 'updated_at'  => $record['updated_at'],
             ]);
